72c60033456a82e46e05c9f01f76120b98bbfe4e
bayardozelaya <bayardozelaya@gmail.com>
2025-04-01 20:35:47 -0400

    deploy record for dns

--- infra/modules/dns/main.tf
+++ infra/modules/dns/main.tf
@@ -4,3 +4,10 @@ resource "google_dns_managed_zone" "primary" {
   description = "Managed by Terraform"
 }
 
+resource "google_dns_record_set" "test_record" {
+  name         = "test.${var.gcp_dns_zone_dns_name}"
+  managed_zone = google_dns_managed_zone.primary.name
+  type         = "A"
+  ttl          = 300
+  rrdatas      = [ "8.8.8.8" ]  
+}
